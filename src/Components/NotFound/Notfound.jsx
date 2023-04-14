7eb4d08d66d537752b103864e72d783f369ab1e3
mohamedashraf14 <moooooooza.1996@gmail.com>
2023-04-14 22:25:08 +0200

    handle not login case

--- src/Components/NotFound/Notfound.jsx
+++ src/Components/NotFound/Notfound.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function Notfound() {
   return (
-      <div className="container">
-        <h2 className='py-2'>Not Found</h2>
-        <p >This page doesn't exist.</p>
-      </div>
-    )
+    <div className="container">
+      <h2 className="py-2">Not Found</h2>
+      <p>This page doesn't exist. if you are not login, Try to login</p>
+    </div>
+  );
 }
--- src/Components/ProtectedRoute/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
+import React from "react";
+import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute(x) {
-  if (localStorage.getItem('userToken')==null){
-return <Navigate to={"/Login"} />
-  }
-  else return x.children
+  if (localStorage.getItem("userToken") == null) {
+    return <Navigate to={"/login"} />;
+  } else return x.children;
 }
